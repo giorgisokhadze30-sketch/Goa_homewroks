89c73e2f1ac7943f34de1d770f26e51ebf09e018
giorgisokhadze30-sketch <giorgisokhadze30@gmail.com>
2026-03-12 15:57:56 +0000

    commit

---
+++ level 1/classwork/cw.py
@@ -0,0 +1,25 @@
+# 1) შექმენით ოთხი სხვადასხვა ცვლადი:
+# • name
+# • surname
+# • age
+# • height
+
+# ცვლადში შინახეთ ტქვენი ინფორმაცია და საბოლოოდ print()- ის საშუალებით გამოიტანეთ მნიშვნელობები ეკრანზე. თითოეულ
+# ცვლადს გვერდზე კომენტარის სახით მიუწერეთ თუ რა მონაცემის ტიპია (მაგ. #string...) 
+
+
+
+
+name = 'Giorgi' 
+# string
+surname = "sokhadze"
+# string
+age = 15 
+# interger
+height = 1.66 
+# float
+
+print(name)
+print(surname)
+print(age)
+print(height)
