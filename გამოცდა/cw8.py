15a73d0abdc5770a94e56fcc3cd3651138e0c4e7
giorgisokhadze30-sketch <giorgisokhadze30@gmail.com>
2026-03-28 15:23:05 +0000

    commit

---
+++ გამოცდა/cw8.py
@@ -0,0 +1,13 @@
+# 8)მომზმარებელს შემოატაინეთ ორი ტიცხვი და ეგრიცხვები შეინახეთ ცვლადებში 
+# და შემდეგ მაგათზე შეასრულე ეს მათემატიკური ოპერაციები: +, -, *, /, **, 
+
+num1 =int( input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1**num2)
