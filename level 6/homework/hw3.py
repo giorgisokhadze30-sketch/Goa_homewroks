e98a42b2926c728403051a290ff029b69f8f6650
giorgisokhadze30-sketch <giorgisokhadze30@gmail.com>
2026-04-08 18:50:30 +0100

    commit

---
+++ level 6/homework/hw3.py
@@ -0,0 +1,8 @@
+# 3) ცვლადებში შეინახეთ თქვენი სახელი, გვარი და ასაკი. ეცადეთ გამოიტანოთ წინადადება "I am NAME SURNAME 
+# and i am YEARS years old" კონკატინაციით (f string-ის გამოყენების გარეშე). 
+# დააკვირდით ტერმინალში შედეგს და ეცადეთ გამოასწოროთ პრობლემა დღეს ნასწავლი მასალის გამოყენებით.
+
+name = "Giorgi"
+surname = "Sokhadze"
+age = 16
+print(f"i am {name} {surname} and i am  {age} years old")
---
+++ level 6/homework/hw5.py
@@ -0,0 +1,15 @@
+# 5) მომხმარებელს შემოატანინეთ 5 რიცხვი. დაწერეთ პროგრამა, რომელიც გამოთვლის და დაბეჭდავს 
+# ამ რიცხვების საშუალო არითმეტიკულს. 
+
+#  (საშუალო არითმეტიკული = რიცხვთა ჯამის განაყოფი რიცხვების რაოდენობაზე)
+
+num1 = int(input("Enter First number: "))
+num2 = int(input("Enter Second number: "))
+num3 = int(input("Enter third number"))
+num4 = int(input("Enter fourht number: "))
+num5 = int(input("Enter fifth number: "))
+
+
+average =num1 + num2 + num3 + num4 + num5
+final = average//15
+print(final)
