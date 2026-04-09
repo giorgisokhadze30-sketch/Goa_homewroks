4a785f63e6b0ff90550e05575366ec549fb9827f
giorgisokhadze30-sketch <giorgisokhadze30@gmail.com>
2026-04-09 23:18:33 +0100

    commit

---
+++ level 8/homework/hw6.py
@@ -0,0 +1,11 @@
+# 7) რას გამოიტანს მოცემული კოდები?
+
+# • False or True and True and False
+
+# • True and False or False or True
+
+# • True or True and False or True or False and True or False
+
+# 1.False
+# 2.True
+# 3.True
