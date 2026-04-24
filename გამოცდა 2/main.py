fe0966367f8a69825916e5d7d81284076f182a78
giorgisokhadze30-sketch <giorgisokhadze30@gmail.com>
2026-04-24 21:52:58 +0100

    commit

---
+++ გამოცდა 2/main.py
@@ -0,0 +1,82 @@
+# 1) ჩამოწერეთ ყველა შედარების ოპერატორი და თიტოეულზე გააკეტეთ 2-2 მაგალითი
+
+# > - მეტობა
+num1 = 12
+num2 = 11
+print(num1 > num2)
+
+
+
+num3 = 15
+num4 = 14
+print(num3 > num4)
+
+
+
+# < - ნაკლებობა
+
+num5 = 17
+num6 = 18
+
+print(num5 < num6)
+
+num7 = 19
+num8 = 20
+
+print(num7 < num8)
+
+
+# >= - მეტია ან ტოლია
+
+num9 = 31
+num10 = 30
+num11 = 31
+
+print(num9 >= num2)
+print(num9 >= num11)
+
+
+
+# <= - ნაკლებია ან ტოლია
+
+num12 = 81
+num13 = 91
+num14 = 81
+
+print(num12 <= num13 )
+print(num12 <= num14)
+
+
+
+# == - ტოლია
+
+num15 = 100
+num16 = 100
+
+print(num15 == num16)
+
+
+num17 = 102
+num18 = 102
+
+
+print(num17 == num18)
+
+
+# != - არ უდრის
+
+num18 = 103
+num19 = 104
+
+print(num18 != num19)
+
+
+num30 = 105
+num21 = 107
+
+print(num30 != num21)
+
+
+
+
+
---
+++ გამოცდა 2/main7.py
@@ -0,0 +1,12 @@
+
+# 7)მომხმარებელს შემოატაინეთ input() მეშვეობით რაიმე რიცხვი და შეამოწმეთ ეგ რიცხვი ლუწია თუ კენტი
+
+num1 =int (input("Enter your number: "))
+
+if num1 % 2 == 0:
+    print("Even")
+else:
+    print("odd")
+
+
+
