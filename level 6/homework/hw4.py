80640aebe07f77acc3b6a4eaed87a4b4b387a760
giorgisokhadze30-sketch <giorgisokhadze30@gmail.com>
2026-05-04 21:21:48 +0100

    commit

--- level 6/homework/hw4.py
+++ level 6/homework/hw4.py
@@ -3,7 +3,6 @@
 # • გამოკლება
 # • გამრავლება
 # • გაყოფა
-<<<<<<< HEAD
 # თითოეული შედეგი შეინახეთ განსხვავებულ ცვლადებში და შემდეგ გამოიტანეთ ტერმინალში. 
 
 
@@ -14,15 +13,3 @@ print(int(num1) + int(num2))
 print(int(num1) - int(num2)) 
 print(int(num1) * int(num2))
 print(int(num1) / int(num2))
-=======
-# თითოეული შედეგი შეინახეთ განსხვავებულ ცვლადებში და შემდეგ გამოიტანეთ ტერმინალში.
- 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter Second number: "))
-
-
-print(num1 + num2)
-print(num1-num2)
-print(num1 * num2)
-print(num1 / num2)
->>>>>>> b517936d5d3090d2aee13860d3b4a07ed4368610
