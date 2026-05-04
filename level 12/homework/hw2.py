09b769af01fe9b610367bdc2338d6149fee8a303
giorgisokhadze30-sketch <giorgisokhadze30@gmail.com>
2026-05-04 19:34:28 +0100

    commit

---
+++ level 12/homework/hw2.py
@@ -0,0 +1,8 @@
+# 3) დაწერეთ პროგრამა, რომელიც ამოწმებს input-ით შემოყვანილი რიცხვი ლუწია თუ კენტი.
+
+num1 =int (input("Enter your number: "))
+
+if num1 % 2 == 0:
+    print("Even")
+else:
+    print("odd")
