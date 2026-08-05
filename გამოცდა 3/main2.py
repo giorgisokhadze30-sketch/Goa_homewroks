e676858786140637750c9545ea897b747858c454
giorgisokhadze30-sketch <giorgisokhadze30@gmail.com>
2026-08-05 16:57:13 +0100

    commit

--- გამოცდა 3/main2.py
+++ გამოცდა 3/main2.py
@@ -1,2 +1,16 @@
 # 2)შექმენით ისეთი ფუნქცია რომელიც  შეამოწმებს თუ მოხმარებელის ასაკი მეტია 18 ზე უნდა დაუბეჭდოთ# "you can enter" და თ ნაკლებია 
-# "you are still child"
+# "you are still child
+    ##
+    
+    
+    
+    
+def check_age(age):
+    if age > 18:
+        return "You can enter"
+    else:
+        return "You are still child"
+    
+    
+print(check_age(15))
+print(check_age(19))
--- გამოცდა 3/main5.py
+++ გამოცდა 3/main5.py
@@ -1,2 +1,10 @@
 # 5) დაწერეთ ფუნქცია რომელიც მიიღებს მომხმარებლის სახელს. გვარსა და ასაკს. ფუნქციამ დააბრუნოს მომხმარებლის
 # მონაცემები f სტრინგის გამოყენებით
+
+
+def user_info(name,surname,age):
+    return f"name:{name}, surname:{surname}, age:{age}"
+
+
+
+print(user_info("Giorgi","Sokhadze",16))
